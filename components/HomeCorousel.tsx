815bf51f3952fd383760b82aee36ffa7f8e220ec
AzizbekGayratov <azizbekgairatov@gmail.com>
2024-08-01 13:02:06 +0500

    add button to carousel

--- components/HomeCorousel.tsx
+++ components/HomeCorousel.tsx
@@ -5,7 +5,7 @@ const HomeCorousel = () => {
   return (
     <section className="max-w-[1440px] mx-auto">
       <div className="carousel w-full">
-        <div className="carousel-item w-full">
+        <div id="slide1" className="carousel-item relative w-full">
           <Image
             src="/hero1.jpg"
             alt="corousel1"
@@ -13,8 +13,16 @@ const HomeCorousel = () => {
             height={600}
             className="w-full"
           />
+          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+            <a href="#slide3" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide2" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
         </div>
-        <div className="carousel-item w-full">
+        <div id="slide2" className="carousel-item relative w-full">
           <Image
             src="/hero2.jpg"
             alt="corousel1"
@@ -22,8 +30,16 @@ const HomeCorousel = () => {
             height={600}
             className="w-full"
           />
+          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+            <a href="#slide1" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide3" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
         </div>
-        <div className="carousel-item w-full">
+        <div id="slide3" className="carousel-item relative w-full">
           <Image
             src="/hero3.jpg"
             alt="corousel1"
@@ -31,6 +47,14 @@ const HomeCorousel = () => {
             height={600}
             className="w-full"
           />
+          <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+            <a href="#slide2" className="btn btn-circle">
+              ❮
+            </a>
+            <a href="#slide1" className="btn btn-circle">
+              ❯
+            </a>
+          </div>
         </div>
       </div>
     </section>
